c3105fe90abb81e3ccd56f1e5405b34a322fc548
Sehyun av Kim <avseoul@192.168.1.7>
2017-04-07 12:54:08 -0400

    work in progress

--- ofxSimpleScreenRecorder.h
+++ ofxSimpleScreenRecorder.h
@@ -1,22 +1,27 @@
 #pragma once
 #include "ofMain.h"
 
-//
-// screenCaptureThread.h
-//
-// *ffmpeg must be installed
-// - a. install guide for all platforms
-// https://trac.ffmpeg.org/wiki/CompilationGuide
-//
-// created on Apr, 2017 @FakeLove http://www.fakelove.tv/
-// author - av http://avseoul.net http://kimsehyun.kr
-//
-class screenCaptureThread : public ofThread{
+/*
+ * ofxSimpleScreenRecorder.h
+ *
+ * a simple class which record screen to h.264 mpeg video without significant frame drops
+ *
+ * ffmpeg must be installed
+ * - a. install guide for all platforms
+ * https://trac.ffmpeg.org/wiki/CompilationGuide
+ *
+ * created on Apr, 2017 at fakelove http://www.fakelove.tv/
+ * by av http://avseoul.net http://kimsehyun.kr
+ *
+ */
+
+class ofxSimpleScreenRecorder : public ofThread{
 public:
-    screenCaptureThread(){}
-    ~screenCaptureThread(){
+    ofxSimpleScreenRecorder(){}
+    ~ofxSimpleScreenRecorder(){
         waitForThread(true);
     }
+    
     void setup(int _w, int _h, std::string _path = ""){
         w = _w, h = _h, bffrCount = 0, thrdBffrCount = 0, saveCount = 0;
         initBuffer();
@@ -26,7 +31,7 @@ public:
             pixels[i] = ofPixels();
         
         path = _path == "" ? ofDirectory::ofDirectory(_path).getAbsolutePath() + "/" : _path;
-        std::cout << "SCThread::target path - " << path << endl;
+        std::cout << "ofxSSR::target path - " << path << endl;
         
         threadRunning = false;
         firstFrame = true;
@@ -39,14 +44,14 @@ public:
             startThread();
             threadRunning = true;
         } else
-            std::cout << "SCThread::thread running" << endl;
+            std::cout << "ofxSSR::thread running" << endl;
     }
     
     void stop(){
         if(threadRunning)
             stopTriggered = true;
         else
-            std::cout << "SCThread::thread not running" << endl;
+            std::cout << "ofxSSR::thread not running" << endl;
     }
     
     void beginDraw(){
@@ -61,9 +66,9 @@ public:
         
         bool _save = saveCount%2 == 0 ? true : false;
         if(_save && threadRunning && !stopTriggered){
-            if(!firstFrame){
+            if(!firstFrame)
                 bffr_ping.unmap();
-            }
+            
             firstFrame = false;
             
             fbo.getTexture().copyTo(bffr_ping);
@@ -76,7 +81,7 @@ public:
             bffrCount++;
             
             if(bffrCount >= numBffr) {
-                std::cout<<"SCThread::buffer reaches limit - stopping rendering" << endl << "change numBffr to increase limit";
+                std::cout<<"ofxSSR::buffer reaches limit - stopping rendering" << endl << "change numBffr to increase limit";
                 stop();
             }
         }
@@ -90,10 +95,12 @@ public:
         }
     }
     
-    int numBffr = 9999;
+    void setNumBffr(int _num){
+        numBffr = _num;
+    }
     
 private:
-    int w, h, bffrCount, thrdBffrCount, saveCount;
+    int w, h, bffrCount, thrdBffrCount, saveCount, numBffr = 9999;
     std::string path;
     ofFbo fbo;
     ofBufferObject bffr_ping, bffr_pong;
@@ -150,26 +157,25 @@ private:
         }
         std::string _cmd = "osascript -e 'tell application \"Terminal\" to do script \"cd " + path + " && ffmpeg -start_number 1 -framerate 30 -i render_%04d.png -c:v libx264 -vf fps=30 -pix_fmt yuv420p -preset ultrafast video"+_tstamp+".mp4 && " + _rmfiles + " && exit\"'";
         std::system(_cmd.c_str());
-        std::cout << "SCThread::rendering completed" << endl;
+        std::cout << "ofxSSR::rendering completed" << endl;
     }
     
     void threadedFunction(){
-        std::cout << "SCThread::thread initiated" << endl;
+        std::cout << "ofxSSR::thread initiated" << endl;
         ofSleepMillis(1000);
-        std::cout << "SCThread::thread started" << endl;
+        std::cout << "ofxSSR::thread started" << endl;
         
         while(threadRunning){
-            std::cout<< "SCThread::saved buffers - "  << thrdBffrCount << " ea / stored buffers - " << bffrCount-1 << " ea" << endl;
+            std::cout<< "ofxSSR::saved buffers - "  << thrdBffrCount << " ea / stored buffers - " << bffrCount-1 << " ea" << endl;
             if(stopTriggered)
-                std::cout<< "SCThread::rendering process - " << std::floor((float)thrdBffrCount/(float)(bffrCount-1.f)*10000.f)*.01f << "%" << endl << endl << "//////////////////////////" << endl << "// DO NOT CLOSE THE APP //" << endl << "//////////////////////////" << endl << endl;
+                std::cout<< "ofxSSR::rendering process - " << std::floor((float)thrdBffrCount/(float)(bffrCount-1.f)*10000.f)*.01f << "%" << endl << endl << "//////////////////////////" << endl << "// DO NOT CLOSE THE APP //" << endl << "//////////////////////////" << endl << endl;
             
             std::string _p = getPath(path, thrdBffrCount);
             ofSaveImage(pixels[thrdBffrCount],_p);
             
             thrdBffrCount++;
         }
-        
-        std::cout << "SCThread::thread ended" << endl;
+        std::cout << "ofxSSR::thread ended" << endl;
     }
     
 };
