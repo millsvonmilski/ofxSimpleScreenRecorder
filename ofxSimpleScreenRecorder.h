3f847d03f3f6fa752615b5d98e6c293f82b9abab
Sehyun av Kim <avseoul@192.168.1.7>
2017-04-07 15:01:18 -0400

    cleanup

--- ofxSimpleScreenRecorder.h
+++ ofxSimpleScreenRecorder.h
@@ -4,11 +4,18 @@
 /*
  * ofxSimpleScreenRecorder.h
  *
- * a simple class which record screen to h.264 mpeg video without significant frame drops
+ * a simple oF class which record screen to h.264 mpeg video without significant frame drops ofxSimpleScreenRecorder would be useful when you document your sketch, Documentation is a frustrating process especially if the recorder causes damm frame drops. Those frame drops usually comes from saving process, recorder waits until last frame saved in order not to miss next frame of animation. ofxSimpleScreenRecorder saves frames to buffer and save imgages in buffer apart from main app's update so you won't feel any frame drops while you recording your sketch, what you see is what you will have.
  *
  *
  *
- * - how it works,
+ * - REQUIREMENT
+ *
+ * ofxSimpleScreenRecorder depends on FFmpeg library to convert image sequence to video so FFmpeg should be installed. Follow the instruction on here https://trac.ffmpeg.org/wiki/CompilationGuide
+ *
+ *
+ *
+ * - USAGE,
+ *
  * a. set the capture size and path with setup(width, height, path).
  *    you can specify the path where video will be saved, just make sure put "/" at the last
  *    ex. "local/somewhere/" not "local/somewhere"
@@ -28,17 +35,25 @@
  * e. wait until rendering process done.
  *    do not close the app during the process
  *
- * mainthread : store pixels of drawing to buffer array every bi-frame
- * ofthread   : save stored pixels to png to given path (bin is default) on local drive
+ * f. when process done, ofxSimpleScreenRecorder will open your terminal and run FFmpeg command
+ *
+ * g. folder where the video is will be popped-up
+ *
+ * h. please enjoy the last step of the process, click the red circle at left top on the terminal
+ *
+ * *. mainthread : store pixels of drawing to buffer array every bi-frame
+ * *. ofthread   : save stored pixels png to given path (bin is default) on local drive
  *
  *
  *
- * - ffmpeg must be installed
- * a. install guide for all platforms
- *    https://trac.ffmpeg.org/wiki/CompilationGuide
+ * - TODOs
+ * a. to convert buffer directly to video without process saving images to local storage and also without process openning terminal
+ *
+ *
  *
  * created on Apr, 2017 at fakelove http://www.fakelove.tv/
  * by av http://avseoul.net http://kimsehyun.kr
+ *
  *
  */
 
